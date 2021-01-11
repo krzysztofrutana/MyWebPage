e7ab0738a0e4d4d94ec754dc5606433e652d1431
Krzysztof Rutana <krzysztofrutana@wp.pl>
2021-01-11 12:30:29 +0100

    Add download file options (for current project), start build detail collapse page uder project row onlist.

--- MyWebPage/Controllers/ProjectsController.cs
+++ MyWebPage/Controllers/ProjectsController.cs
@@ -8,18 +8,19 @@ using Microsoft.EntityFrameworkCore;
 using MyWebPage.Data;
 using MyWebPage.Models;
 using MyWebPage.Repositories;
+using MyWebPage.Services;
 
 namespace MyWebPage.Controllers
 {
     public class ProjectsController : Controller
     {
-        private readonly ProjectsDatabaseContext _context;
+        private FileService _fileService;
         private ProjectRepository projectRepository;
 
-        public ProjectsController(ProjectsDatabaseContext context)
+        public ProjectsController(ProjectsDatabaseContext context, FileService fileService)
         {
-            _context = context;
             projectRepository = new ProjectRepository(context);
+            _fileService = fileService;
         }
 
         [HttpGet]
@@ -27,6 +28,13 @@ namespace MyWebPage.Controllers
         {
             var projects = projectRepository.GetAll();
             return PartialView(projects);
+        }
+
+        [HttpGet]
+        [Route("download")]
+        public IActionResult Get(String filePath)
+        {
+            return _fileService.GetFileAsStream(filePath) ?? (IActionResult)NotFound();
         }
     }
 }
---
+++ MyWebPage/Models/FileService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MimeTypes;
+
+namespace MyWebPage.Services
+{
+    public class FileService
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public FileService(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public FileStreamResult GetFileAsStream(String filePath)
+        {
+            try
+            {
+                string wwwPath = _environment.WebRootPath;
+                string fullPath = Path.Combine(wwwPath, filePath);
+
+                var stream = _environment.WebRootFileProvider
+                                .GetFileInfo(filePath)
+                                .CreateReadStream();
+
+                return new FileStreamResult(stream, MimeTypeMap.GetMimeType(fullPath)) { FileDownloadName = Path.GetFileName(filePath)};
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            
+
+            
+
+
+        }
+    }
+}
